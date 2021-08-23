971b2846ca57855b7168e3dadad38457a82cbd83
narang09 <68297005+narang09@users.noreply.github.com>
2021-08-23 17:11:28 +0530

    update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 
-while (($1=="true")) ;
-do
-	echo "my second & third inpur is $2 $3 "
-	
-done
+echo -e "Enter first number: \c"
+read -r x
+echo -e "Enter Second number: \c"
+read -r y
+echo -e "Enter third number: \c"
+read -r z
 
+
+while (($x=="true")) ;
+do
+        sleep 1
+        echo "my second & third inpur is $y $z "
+        echo "press cltrl+c to stop this loop "
+done
 
 
 
